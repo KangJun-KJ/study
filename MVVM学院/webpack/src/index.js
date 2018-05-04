47a0b380305247298eb6cd53dfb1183866efcc91
kangjun <892498546@qq.com>
2018-05-04 18:18:55 +0800

    transition

--- MVVM学院/webpack/src/index.js
+++ MVVM学院/webpack/src/index.js
@@ -9,6 +9,8 @@ import task2_3 from '@/components/task2_3'
 import task2_4 from '@/components/task2_4'
 import task2_5 from '@/components/task2_5'
 import task2_6 from '@/components/task2_6'
+import task2_7_1 from '@/components/task2_7_1'
+import task2_7_2 from '@/components/task2_7_2'
 
 router.add({rule: '/', Component: App, target: '#app'})
 router.add({rule: '/task2_1', Component: task2_1, target: '#app'})
@@ -17,5 +19,7 @@ router.add({rule: '/task2_3', Component: task2_3, target: '#app'})
 router.add({rule: '/task2_4', Component: task2_4, target: '#app'})
 router.add({rule: '/task2_5', Component: task2_5, target: '#app'})
 router.add({rule: '/task2_6', Component: task2_6, target: '#app'})
+router.add({rule: '/task2_7_1', Component: task2_7_1, target: '#app'})
+router.add({rule: '/task2_7_2', Component: task2_7_2, target: '#app'})
 
 router.start();
