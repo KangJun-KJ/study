ce5fff63a5fdab9f8b5040f340eb0df71a60160c
kangjun <892498546@qq.com>
2018-05-08 15:25:53 +0800

    .

--- MVVM学院/webpack/src/index.js
+++ MVVM学院/webpack/src/index.js
@@ -1,7 +1,6 @@
 import San from 'san'
 import {router,Link} from 'san-router'
 
-
 import App from '@/App'
 import task2_1 from '@/components/task2_1'
 import task2_2 from '@/components/task2_2'
@@ -12,6 +11,8 @@ import task2_6 from '@/components/task2_6'
 import task2_7_1 from '@/components/task2_7_1'
 import task2_7_2 from '@/components/task2_7_2'
 import task2_7_2_demo from '@/components/task2_7_2_demo'
+import task2_8 from '@/components/task2_8'
+import task3_1 from "@/components/task3_1"
 
 router.add({rule: '/', Component: App, target: '#app'})
 router.add({rule: '/task2_1', Component: task2_1, target: '#app'})
@@ -23,5 +24,7 @@ router.add({rule: '/task2_6', Component: task2_6, target: '#app'})
 router.add({rule: '/task2_7_1', Component: task2_7_1, target: '#app'})
 router.add({rule: '/task2_7_2', Component: task2_7_2, target: '#app'})
 router.add({rule: '/task2_7_2_demo', Component: task2_7_2_demo, target: '#app'})
+router.add({rule: '/task2_8', Component: task2_8, target: '#app'})
+router.add({rule: '/task3_1', Component: task3_1, target: '#app'})
 
 router.start();
